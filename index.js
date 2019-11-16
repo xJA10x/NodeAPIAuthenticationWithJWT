822018c4acc6c7001f4e50c30723e187bf2ad353
jhoset10 <jhosetceron@aol.com>
2019-11-15 20:14:41 -0500

    Made some changes to the comments

--- index.js
+++ index.js
@@ -3,19 +3,21 @@ const express = require('express');
 // Initializes express app.
 const app = express();
 
+// Endpoint
 app.get('/about', (req, res) => {
 
   res.send("We are on the About page")
 
 });
 
+// Endpoint
 app.get('/projects', (req, res) => {
 
   res.send('We are on the project page')
 
-
 });
 
+// Endpoint
 app.get('/Contact', (req, res) => {
 
   res.send("We are on the contact page")
